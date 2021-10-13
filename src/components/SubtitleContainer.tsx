d3189312df8ef0c71ad87d26af7c0d0b0ac8171d
Marc Casamitjana Montseny <mcmontseny.dev@gmail.com>
2021-10-13 23:20:42 +0200

    Add timer + call to subtitles api

--- src/components/SubtitleContainer.tsx
+++ src/components/SubtitleContainer.tsx
@@ -6,15 +6,16 @@ import { play, time } from "ionicons/icons";
 
 interface ContainerProps {
   fileName: string;
+  timeFormat: string;
 }
 
-const SubtitleContainer: React.FC<ContainerProps> = ({ fileName }) => {
+const SubtitleContainer: React.FC<ContainerProps> = ({ fileName, timeFormat }) => {
 
   const txtEnglish = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
   const txtSpanish = "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.";
   return (
     <div className="container">
-      <p><IonIcon icon={play}/> {fileName}    <IonIcon icon={time}/> 35:28</p>
+      <p><IonIcon icon={play}/> {fileName}    <IonIcon icon={time}/> {timeFormat}</p>
       <SubtitleItem backgroundColor="white" language="Inglés" content={txtEnglish} />
       <SubtitleItem backgroundColor="blue" language="Español" content={txtSpanish} />
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,11 +9,45 @@ import {
   IonIcon,
 } from "@ionic/react";
 import { save, sync, play } from "ionicons/icons";
+import { useState, useEffect } from "react";
+
+import axios from 'axios';
+import moment from 'moment';
 
 import SubtitleContainer from "../components/SubtitleContainer";
 import "./Home.css";
 
 const Home: React.FC = () => {
+
+  const [time, setTime] = useState(0);
+  const [timeFormat, setTimeFormat] = useState('');
+
+  const getSubtitles = async () => {
+    try {
+      const res = await axios.get('localhost:8000/api/v1/subs');
+      console.log(res);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  
+  useEffect(() => {
+    (async () => {
+      await getSubtitles();
+    })(); 
+    const startTimestamp = moment().startOf("day");
+    let time = 0;
+
+    const timer = setInterval(() => {
+      setTimeFormat(startTimestamp.format('HH:mm:ss'));
+      setTime(time);
+      startTimestamp.add(1, 'second');
+      time++;
+    }, 1000);
+
+    return clearInterval(timer);
+  }, []);
+
   return (
     <IonPage>
       <IonHeader color="warning">
@@ -37,13 +71,13 @@ const Home: React.FC = () => {
           </IonFabButton>
         </IonFab>
 
-        <IonFab vertical="bottom" horizontal="start" slot="fixed">
+        <IonFab vertical="bottom" horizontal="start" slot="fixed" onClick={() => alert('hola')}>
           <IonFabButton color="warning">
             <IonIcon icon={play} />
           </IonFabButton>
         </IonFab>
         <div className="container">
-          <SubtitleContainer fileName="peaky_blinders_cap12.mp4" />
+          <SubtitleContainer fileName="peaky_blinders_cap12.mp4" timeFormat={timeFormat} />
         </div>
       </IonContent>
     </IonPage>
